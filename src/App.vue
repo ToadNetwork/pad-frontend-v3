672ae0cfefc98b4b264f307ae1c68422a959b89a
Slumdog Shillionaire <slumdog.shillionaire@protonmail.com>
2021-12-07 19:04:52 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      dark
+      outlined="false"
+    >
+      <div>
+        <v-img
+          src="./assets/logo-white.png"
+          width="162.42px"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <div class="d-flex padswap-navbar">
+        <div>Toad<v-icon>mdi-chevron-down</v-icon></div>
+        <div>Padswap<v-icon>mdi-chevron-down</v-icon></div>
+        <div>Shop<v-icon>mdi-chevron-down</v-icon></div>
+        <div>Games<v-icon></v-icon></div>
+        <div>About<v-icon>mdi-chevron-down</v-icon></div>
+      </div>
+      <div class="d-flex padswap-navbar ml-4">
+        <!-- TODO: pad icon -->
+        <div style="color: #00FC4C;">
+          $0.000013
+        </div>
+        <div>EN<v-icon>mdi-chevron-down</v-icon></div>
+        <div class="padswap-address-box">0x1...289</div>
+      </div>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+});
+</script>
+
+<style scoped>
+@font-face {
+  font-family: Roboto Mono;
+  src: url(assets/font/RobotoMono-VariableFont_wght.ttf);
+}
+
+.v-application {
+  font-family: Roboto Mono;
+  letter-spacing: 0.25px;
+  background-image: url(assets/BG.png) !important;
+  background-color: #000000 !important;
+  background-repeat: no-repeat !important;
+  padding: 0px 35px;
+}
+.v-sheet.v-toolbar.v-app-bar {
+  padding-left: 120px;
+  padding-right: 120px;
+  background-color: transparent !important;
+  box-shadow: none;
+  border: none;
+}
+
+.padswap-navbar div {
+  padding-right: 25px;
+}
+.padswap-address-box {
+  border: 1.5px solid #00FC4C;
+  box-sizing: border-box;
+  border-radius: 100px;
+  line-height: 20px;
+  padding: 0px 20px;
+  /* TODO: bigger font and vertical height */
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,197 @@
+<template>
+  <v-container class="padswap-header-box">
+    <div class="padswap-farm-picker" style="margin-bottom: 70px;">
+      <div class="padswap-farm-picker-left padswap-toad-picker">Toad</div>
+      <div class="padswap-farm-picker-right padswap-pad-picker">Pad</div>
+    </div>
+    <v-img src="../assets/Toad Pad.png" />
+    <div style="font-size: 36px; margin-top: 20px;">Pad Farms</div>
+    <div style="font-size: 14px; color: #B3B8C1; margin-top: 10px;">Stake tokens to earn PAD, LP and partner tokens.</div>
+    <v-sheet style="margin-top: 60px;">
+    <v-card>
+      <div class="d-flex align-center justify-space-between">
+        <div class="d-flex align-center">
+        <div class="padswap-farm-picker">
+          <div class="padswap-farm-picker-left padswap-v2farms-picker">
+            V2 Farms
+          </div>
+          <div class="padswap-farm-picker-right padswap-retiringfarms-picker">
+            Retiring
+          </div>
+        </div>
+        <div class="d-flex align-center ml-5">
+          <v-switch /> Staked
+        </div>
+        </div>
+        <div class="d-flex align-center">
+          Sort by:
+          <v-select
+            solo
+            hide-details="true"
+            class="ml-2"
+            background-color="#292D38"
+            rounded
+            :items="['Hot']"
+            value="Hot"
+          />
+          <div class="mx-3"></div>
+          Search
+          <v-text-field
+           solo
+           hide-details="true"
+           background-color="#292D38"
+           rounded
+           class="ml-2"
+           placeholder="Enter Token Name"
+           append-icon="mdi-magnify"
+          />
+        </div>
+      </div>
+    </v-card>
+    <v-card class="padswap-farms mt-4 pa-3">
+      <div class="mx-5 padswap-farm-title padswap-farm-title-shadow mb-4">Regular Farms</div>
+
+      <v-card
+        v-for="i in 6"
+        :key="i"
+        class="d-flex align-center justify-space-between"
+      >
+        <div class="d-flex align-center">
+          <v-img src="../assets/tokens/BNB.png" class="padswap-token-img" />
+          <v-img src="../assets/tokens/BUSD.png" class="padswap-token-img mr-4" style="margin-left: -5px;" />
+          <div class="padswap-farm-title">BNB-BUSD</div>
+        </div>
+        <div class="d-flex align-center justify-space-between" style="width: 45%;">
+          <div class="d-flex" style="flex-direction: column;">
+            <div class="padswap-farm-data-title">YEARLY APY</div>
+            <div class="padswap-farm-data-item">417.04%</div>
+          </div>
+          <div class="d-flex" style="flex-direction: column;">
+            <div class="padswap-farm-data-title">DAILY ROI</div>
+            <div class="padswap-farm-data-item">0.45%</div>
+          </div>
+          <div class="d-flex" style="flex-direction: column;">
+            <div class="padswap-farm-data-title">PAD EARNED</div>
+            <div class="padswap-farm-data-item">~</div>
+          </div>
+        </div>
+        <div>
+          <v-btn
+            rounded
+            class="padswap-farm-btn mr-5"
+          >
+            Enable
+          </v-btn>
+          <v-icon color="#00FC4C">mdi-chevron-down</v-icon>
+        </div>
+      </v-card>
+    </v-card>
+    </v-sheet>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    name: 'Home'
+  })
+</script>
+
+<style scoped>
+.v-sheet {
+  width: 100%;
+  background-color: #000000;
+}
+.v-card {
+  background: #181D26;
+  border-radius: 15px !important;
+  width: 100%;
+  color: #B3B8C1 !important;
+  padding: 5px 30px;
+  font-family: Roboto;
+}
+.v-select {
+  border-radius: 8px;
+}
+.v-text-field {
+  border-radius: 8px;
+}
+
+.padswap-header-box {
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.padswap-farm-picker {
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  background: #FFFFFF;
+  box-sizing: border-box;
+  border-radius: 100px;
+  font-size: 14px;
+  font-weight: bold;
+}
+.padswap-toad-picker {
+  color: #595E67;
+  padding: 10px 29px;
+}
+.padswap-pad-picker {
+  background: linear-gradient(180deg, #F99DF3 0%, #FA77F1 100%);
+  color: #920087;
+  border-radius: 100px;
+  padding: 10px 28px;
+}
+.padswap-v2farms-picker {
+  color: #181D26;
+  padding: 7px 15px;
+}
+.padswap-retiringfarms-picker {
+  color: #879CA5;
+  background: #292D38;
+  border-radius: 100px;
+  padding: 7px 15px;
+  font-weight: 500;
+}
+.padswap-farm-title {
+  font-family: Roboto Mono;
+  color: #FB53EF;
+  font-size: 16px;
+}
+.padswap-farm-title-shadow {
+  text-shadow: 0px 0px 4px rgba(250, 64, 237, 0.5);
+}
+.v-card.padswap-farms {
+  background: rgba(255, 255, 255, 0.05);
+}
+.padswap-farms .v-card {
+  padding-top: 30px;
+  padding-bottom: 30px;
+  margin-bottom: 5px;
+}
+.padswap-token-img {
+  height: 30px;
+  width: 30px;
+  max-width: 30px;
+}
+.padswap-farm-btn {
+  border-radius: 100px;
+  background: linear-gradient(180deg, #00FC4c 0%, #00D741 100%);
+  color: #00310F;
+  width: 120px;
+  text-transform: none;
+  font-size: 16px;
+  font-weight: bold;
+  font-family: Roboto Mono;
+}
+.padswap-farm-data-title {
+  font-size: 12px;
+  color: #71767F;
+}
+.padswap-farm-data-item {
+  font-size: 24px;
+  color: #FFFFFF;
+}
+</style>
