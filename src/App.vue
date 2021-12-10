8e8231a1470b1ea41a9c7ea0b8eb5f945f70cbd6
Slumdog Shillionaire <slumdog.shillionaire@protonmail.com>
2021-12-10 15:36:20 -0800

    Add remaining navigation menus

--- src/App.vue
+++ src/App.vue
@@ -15,69 +15,159 @@
       <v-spacer></v-spacer>
 
       <div class="d-flex padswap-navbar">
-        <div>Toad<v-icon>mdi-chevron-down</v-icon></div>
-        <v-menu offset-y>
-          <template v-slot:activator="{ on, attrs }">
-            <div
-              v-bind="attrs"
-              v-on="on"
-            >
-              <span>Padswap</span>
-              <v-icon>mdi-chevron-down</v-icon>
-            </div>
-          </template>
-          <v-list class="padswap-nav-menu">
-            <!-- TODO: separate component -->
-            <v-list-item>
-              <v-list-item-icon>
-                <v-img src="./assets/icons/Icon-Swap.svg" />
-              </v-list-item-icon>
-              <v-list-item-content>
-                <div>Swap</div>
-                <v-subheader>Buy or sell tokens on PadSwap</v-subheader>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-icon>
-                <v-img src="./assets/icons/Icon-Pad.svg" />
-              </v-list-item-icon>
-              <v-list-item-content>
-                <div>Pad Farms</div>
-                <v-subheader>Earn tokens by providing liquidity</v-subheader>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-icon>
-                <v-img src="./assets/icons/Icon-Pad.svg" />
-              </v-list-item-icon>
-              <v-list-item-content>
-                <div>Add Liquidity</div>
-                <v-subheader>Create LP tokens to add to farms</v-subheader>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-icon>
-                <v-img src="./assets/icons/Vault.svg" />
-              </v-list-item-icon>
-              <v-list-item-content>
-                <div>Vault</div>
-                <v-subheader>PAD's backing reserves</v-subheader>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-list-item-icon>
-                <v-img src="./assets/icons/Icon-Chart.svg" />
-              </v-list-item-icon>
-              <v-list-item-content>
-                <div>Stats</div>
-                <v-subheader>Analyze token prices and volume</v-subheader>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-        <div>Shop<v-icon>mdi-chevron-down</v-icon></div>
-        <div>Games<v-icon></v-icon></div>
-        <div>About<v-icon>mdi-chevron-down</v-icon></div>
+        <nav-menu title="Toad">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-News.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>News</div>
+              <v-subheader>Latest news and announcements</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Toad.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Toad Farms</div>
+              <v-subheader>Earn tokens by providing liquidity</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Church.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Temple</div>
+              <v-subheader>Convert non-believers into hodlers</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+        </nav-menu>
+        <nav-menu title="Padswap">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Swap.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Swap</div>
+              <v-subheader>Buy or sell tokens on PadSwap</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Pad.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Pad Farms</div>
+              <v-subheader>Earn tokens by providing liquidity</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Pad.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Add Liquidity</div>
+              <v-subheader>Create LP tokens to add to farms</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Vault.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Vault</div>
+              <v-subheader>PAD's backing reserves</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Chart.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Stats</div>
+              <v-subheader>Analyze token prices and volume</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+        </nav-menu>
+        <nav-menu title="Shop">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Piramyd.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Piramyd Store</div>
+              <v-subheader>Official Toad Merch</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Amazon.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Amazon Store</div>
+              <v-subheader>Official Toad Merch</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+        </nav-menu>
+        <nav-menu title="Games" non-expandable />
+        <nav-menu title="About">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Wiki.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>TOAD Wiki</div>
+              <v-subheader>Learn more about TOAD and PAD</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Academy.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Toad Academy</div>
+              <v-subheader>Learn more about crypto</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Audit.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Toad Network Audit Report</div>
+              <v-subheader>Contracts audit report by SpaceAudits</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Audit.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>PadSwap Audit Report</div>
+              <v-subheader>Contracts audit report by SpaceAudits</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Paper.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>Toad Network White Paper</div>
+              <v-subheader>Toad Network's White Paper</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-img src="./assets/icons/Icon-Paper.svg" />
+            </v-list-item-icon>
+            <v-list-item-content>
+              <div>PadSwap White Paper</div>
+              <v-subheader>PadSwap's White Paper</v-subheader>
+            </v-list-item-content>
+          </v-list-item>
+        </nav-menu>
       </div>
       <div class="d-flex padswap-navbar ml-4">
         <!-- TODO: pad icon -->
@@ -96,15 +186,17 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
+import Vue from 'vue'
+import NavMenu from './components/NavMenu.vue'
 
 export default Vue.extend({
   name: 'App',
+  components: { NavMenu },
 
   data: () => ({
     //
   }),
-});
+})
 </script>
 
 <style scoped>
@@ -137,23 +229,5 @@ export default Vue.extend({
   line-height: 20px;
   padding: 0px 20px;
   /* TODO: bigger font and vertical height */
-}
-
-.padswap-nav-menu {
-  border-radius: 8px !important;
-  background-color: #000000 !important;
-}
-.padswap-nav-menu .v-list-item {
-  padding: 11px 25px;
-  font-family: Roboto;
-}
-.padswap-nav-menu .v-list-item > div {
-  margin-bottom: 0px;
-}
-.padswap-nav-menu .v-list-item .v-subheader {
-  padding: 0px;
-  margin-top: 2px;
-  height: auto;
-  font-size: 12px;
 }
 </style>
---
+++ src/components/NavMenu.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-menu
+    offset-y
+    :disabled="nonExpandable"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <div
+        v-bind="attrs"
+        v-on="on"
+        class="padswap-navbar-item"
+      >
+        <span>{{ title }}</span>
+        <v-icon v-show="!nonExpandable">mdi-chevron-down</v-icon>
+      </div>
+    </template>
+    <v-list class="padswap-nav-menu">
+      <slot></slot>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'NavMenu',
+  props: {
+    title: String,
+    nonExpandable: Boolean
+  }
+})
+</script>
+
+<style scoped>
+.padswap-navbar-item {
+  padding-right: 25px;
+}
+.padswap-nav-menu {
+  border-radius: 8px !important;
+  background-color: #000000 !important;
+}
+.padswap-nav-menu /deep/ .v-list-item {
+  padding: 11px 18px;
+  font-family: Roboto;
+  margin: 0px 7px;
+}
+.padswap-nav-menu /deep/ .v-list-item:hover .v-image,
+.padswap-nav-menu /deep/ .v-list-item:hover .v-list-item__content > div:first-child {
+  filter: invert(68%) sepia(70%) saturate(3531%) hue-rotate(94deg) brightness(108%) contrast(117%);
+}
+.padswap-nav-menu /deep/ .v-list-item:hover .v-subheader {
+  color: #EBEDEF;
+}
+.padswap-nav-menu /deep/ .v-list-item:hover {
+  cursor: pointer;
+  background: #181D26;
+  border-radius: 6px;
+}
+.padswap-nav-menu /deep/ .v-list-item > div {
+  margin-bottom: 0px;
+}
+.padswap-nav-menu /deep/ .v-list-item__content {
+  flex-direction: column;
+  align-items: flex-start;
+}
+.padswap-nav-menu /deep/ .v-list-item__content > div {
+  display: flex;
+}
+.padswap-nav-menu /deep/ .v-list-item .v-subheader {
+  padding: 0px;
+  margin-top: 2px;
+  height: auto;
+  font-size: 12px;
+}
+</style>
